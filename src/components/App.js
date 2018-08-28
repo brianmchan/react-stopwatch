43233fae91884b78dfccef79b7584b0e8f829a9b
Brian Chan <bchan92@uw.edu>
2018-08-27 20:12:01 -0700

    Implement Stopwatch functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Stopwatch from './Stopwatch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React Stopwatch</h1>
+        <Stopwatch />
       </div>
     );
   }
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+export default class Stopwatch extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            runningTime: 0,
+            status: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    handleClick () {
+        this.setState(state => {
+            if (state.status) {
+                clearInterval(this.timer);
+            } else {
+                const startTime = Date.now() - this.state.runningTime;
+                
+                this.timer = setInterval(() => {
+                    this.setState({ runningTime: Date.now() - startTime });
+                });
+            }
+
+            return { status: !state.status };
+        });
+    }
+
+    handleReset () {
+        clearInterval(this.timer);
+        
+        this.setState({
+            runningTime: 0,
+            status: false
+        });
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timer);
+    }
+
+    render () {
+        const { runningTime, status } = this.state;
+
+        return (
+            <div className='stopwatch'>
+                <p>{ runningTime } ms</p>
+                <button onClick={this.handleClick}>{ status ? 'Stop' : 'Start' }</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        )
+    }
+}
